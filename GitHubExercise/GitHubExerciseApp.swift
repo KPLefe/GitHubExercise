1b2fc617ecc64096a63e6459d195f1dee3991c54
Kevin Lefebvre <kevin@gbamerica.net>
2025-01-17 11:29:04 -0500

    Initial Commit

---
+++ GitHubExercise/GitHubExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubExerciseApp.swift
+//  GitHubExercise
+//
+//  Created by Kevin Lefebvre on 1/17/25.
+//
+
+import SwiftUI
+
+@main
+struct GitHubExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
